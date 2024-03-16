ec5f0b5980a0260492f4c4d69c2ccc65703042b6
Alexander Dadyka <sanekokokok@gmail.com>
2024-03-16 18:11:21 +0300

    Add: DatabaseProvider struct

---
+++ internal/providers/database_provider.go
@@ -0,0 +1,50 @@
+package providers
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
+
+type DatabaseProvider struct {
+}
+
+func NewDatabaseProvider() *DatabaseProvider {
+	return &DatabaseProvider{}
+}
+
+func (databaseProvider *DatabaseProvider) InitGorm() *gorm.DB {
+	const sslMode = "disable"
+
+	timezone := viper.GetString("DB_TIME_ZONE")
+
+	dataSourceName := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		viper.GetString("DB_HOST"),
+		viper.GetString("DB_USERNAME"),
+		viper.GetString("DB_PASSWORD"),
+		viper.GetString("DB_DATABASE"),
+		viper.GetString("DB_PORT"),
+		sslMode,
+		timezone,
+	)
+
+	dialector := postgres.Open(dataSourceName)
+
+	gormDB, err := gorm.Open(dialector, &gorm.Config{
+		SkipDefaultTransaction: true,
+		NowFunc: func() time.Time {
+			location, _ := time.LoadLocation(timezone)
+			return time.Now().In(location)
+		},
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to the Database")
+	}
+
+	return gormDB
+}
