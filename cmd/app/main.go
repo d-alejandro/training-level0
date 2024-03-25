ac02151c01afb2994f0fa5ed6e283763a089a7f1
Alexander Dadyka <sanekokokok@gmail.com>
2024-03-26 01:51:44 +0300

    Fix: cmd/app/main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/d-alejandro/training-level0/internal/migrations"
+	"github.com/d-alejandro/training-level0/internal/nats_streaming"
 	"github.com/d-alejandro/training-level0/internal/providers"
+	"github.com/nats-io/stan.go"
+	"github.com/spf13/viper"
 	"log"
+	"time"
 )
 
 func main() {
@@ -16,6 +21,19 @@ func main() {
 	orderModelsTableMigration := migrations.NewOrderModelsTableMigration(gorm)
 	err := orderModelsTableMigration.Migrate()
 	if err != nil {
-		log.Fatal("Failed to complete migrations")
+		log.Fatal("Failed to complete migrations.")
 	}
+
+	clientID := viper.GetString("CLIENT_ID_SUBSCRIBER")
+	stanConnector := nats_streaming.NewStanConnector(clientID)
+
+	stanConnection := stanConnector.CreateConnection()
+	defer stanConnector.CloseConnection(stanConnection)
+
+	stanSubscriber := nats_streaming.NewStanSubscriber(stanConnection)
+	stanSubscriber.Subscribe(func(message *stan.Msg) {
+		fmt.Println(string(message.Data))
+	})
+
+	time.Sleep(60 * time.Second)
 }
