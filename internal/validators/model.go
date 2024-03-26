fbec60d5b2ee9a2368a554d499ab960cb8def1bf
Alexander Dadyka <sanekokokok@gmail.com>
2024-03-26 21:03:37 +0300

    Add: validation tags to Model structure

--- internal/validators/model.go
+++ internal/validators/model.go
@@ -1,51 +1,49 @@
 package validators
 
-import "time"
-
 type Model struct {
-	OrderUid    string `json:"order_uid"`
-	TrackNumber string `json:"track_number"`
-	Entry       string `json:"entry"`
+	OrderUID    string `json:"order_uid" validate:"required"`
+	TrackNumber string `json:"track_number" validate:"required"`
+	Entry       string `json:"entry" validate:"required"`
 	Delivery    struct {
-		Name    string `json:"name"`
-		Phone   string `json:"phone"`
-		Zip     string `json:"zip"`
-		City    string `json:"city"`
-		Address string `json:"address"`
-		Region  string `json:"region"`
-		Email   string `json:"email"`
-	} `json:"delivery"`
+		Name    string `json:"name" validate:"required"`
+		Phone   string `json:"phone" validate:"required,e164"`
+		Zip     string `json:"zip" validate:"required"`
+		City    string `json:"city" validate:"required"`
+		Address string `json:"address" validate:"required"`
+		Region  string `json:"region" validate:"required"`
+		Email   string `json:"email" validate:"required,email"`
+	} `json:"delivery" validate:"required"`
 	Payment struct {
-		Transaction  string `json:"transaction"`
-		RequestId    string `json:"request_id"`
-		Currency     string `json:"currency"`
-		Provider     string `json:"provider"`
-		Amount       int    `json:"amount"`
-		PaymentDt    int    `json:"payment_dt"`
-		Bank         string `json:"bank"`
-		DeliveryCost int    `json:"delivery_cost"`
-		GoodsTotal   int    `json:"goods_total"`
-		CustomFee    int    `json:"custom_fee"`
-	} `json:"payment"`
+		Transaction  string `json:"transaction" validate:"required"`
+		RequestID    string `json:"request_id" validate:"omitempty,min=1"`
+		Currency     string `json:"currency" validate:"required,iso4217"`
+		Provider     string `json:"provider" validate:"required"`
+		Amount       int    `json:"amount" validate:"required,numeric,min=0"`
+		PaymentDT    int    `json:"payment_dt" validate:"required,numeric,min=946684800"`
+		Bank         string `json:"bank" validate:"required"`
+		DeliveryCost int    `json:"delivery_cost" validate:"required,numeric,min=0"`
+		GoodsTotal   int    `json:"goods_total" validate:"required,numeric,min=0"`
+		CustomFee    int    `json:"custom_fee" validate:"required|eq=0,numeric"`
+	} `json:"payment" validate:"required"`
 	Items []struct {
-		ChrtId      int    `json:"chrt_id"`
-		TrackNumber string `json:"track_number"`
-		Price       int    `json:"price"`
-		Rid         string `json:"rid"`
-		Name        string `json:"name"`
-		Sale        int    `json:"sale"`
-		Size        string `json:"size"`
-		TotalPrice  int    `json:"total_price"`
-		NmId        int    `json:"nm_id"`
-		Brand       string `json:"brand"`
-		Status      int    `json:"status"`
-	} `json:"items"`
-	Locale            string    `json:"locale"`
-	InternalSignature string    `json:"internal_signature"`
-	CustomerId        string    `json:"customer_id"`
-	DeliveryService   string    `json:"delivery_service"`
-	Shardkey          string    `json:"shardkey"`
-	SmId              int       `json:"sm_id"`
-	DateCreated       time.Time `json:"date_created"`
-	OofShard          string    `json:"oof_shard"`
+		ChrtID      int    `json:"chrt_id" validate:"required,numeric,min=0"`
+		TrackNumber string `json:"track_number" validate:"required"`
+		Price       int    `json:"price" validate:"required,numeric,min=0"`
+		Rid         string `json:"rid" validate:"required"`
+		Name        string `json:"name" validate:"required"`
+		Sale        int    `json:"sale" validate:"required,numeric,min=0"`
+		Size        string `json:"size" validate:"required"`
+		TotalPrice  int    `json:"total_price" validate:"required,numeric,min=0"`
+		NmID        int    `json:"nm_id" validate:"required,numeric,min=0"`
+		Brand       string `json:"brand" validate:"required"`
+		Status      int    `json:"status" validate:"required,numeric,min=0"`
+	} `json:"items" validate:"dive"`
+	Locale            string `json:"locale" validate:"required,min=2,bcp47_language_tag"`
+	InternalSignature string `json:"internal_signature" validate:"omitempty,min=1"`
+	CustomerID        string `json:"customer_id" validate:"required"`
+	DeliveryService   string `json:"delivery_service" validate:"required"`
+	Shardkey          string `json:"shardkey" validate:"required"`
+	SmID              int    `json:"sm_id" validate:"required,numeric,min=0"`
+	DateCreated       string `json:"date_created" validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
+	OofShard          string `json:"oof_shard" validate:"required"`
 }
