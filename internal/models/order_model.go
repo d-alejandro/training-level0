582310018c6e2b3d2f1ff403950b19bca16c3fb8
Alexander Dadyka <sanekokokok@gmail.com>
2024-03-26 21:50:18 +0300

    Fix: OrderModel structure

--- internal/models/order_model.go
+++ internal/models/order_model.go
@@ -1,13 +1,12 @@
 package models
 
 import (
-	"github.com/d-alejandro/training-level0/internal/models/types"
 	"time"
 )
 
 type OrderModel struct {
-	ID        uint             `gorm:"primaryKey"`
-	Model     types.ModelJSONB `gorm:"type:jsonb;not null;index:idx_order_models_model_order_uid,unique,expression:(model->>'order_uid')"`
-	CreatedAt *time.Time       `gorm:"type:timestamp(0)"`
-	UpdatedAt *time.Time       `gorm:"type:timestamp(0)"`
+	ID        uint       `gorm:"primaryKey"`
+	Model     Model      `gorm:"type:jsonb;not null;index:idx_order_models_model_order_uid,unique,expression:(model->>'order_uid')"`
+	CreatedAt *time.Time `gorm:"type:timestamp(0)"`
+	UpdatedAt *time.Time `gorm:"type:timestamp(0)"`
 }
